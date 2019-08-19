504cea62dc5f8ffc6870384f2656a19c75873ab1
Ferret56 <Ferret56@yandex.ru>
2019-08-19 13:02:59 +0300

    create Flower

---
+++ src/main/java/com/Ferret56/FlowerShopEE/be/entity/Flower/Flower.java
@@ -0,0 +1,60 @@
+package com.Ferret56.FlowerShopEE.be.entity.Flower;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "FLOWERS")
+public class Flower {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "amount")
+    private int amount;
+
+    public Flower() {
+    }
+
+    public Flower(String name, BigDecimal price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
